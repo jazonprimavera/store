c9af6c96770167967b370a6c83779cdb89a499fa
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-11 11:52:12 +0800

    Delete cofig.php

--- cofig.php
+++
@@ -1,8 +0,0 @@
-  
-<?php
-session_start();
-define('DB_SERVER','dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com');
-define('DB_USERNAME','admin');
-define('DB_PASSWORD','root1234');
-define('DB_DATABASE','db_1820521');
-?>
