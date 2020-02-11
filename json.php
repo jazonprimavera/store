a402f8f689c320a0be008a12b8c6dc4702e81c2d
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-11 14:43:26 +0800

    Update json.php

--- json.php
+++ json.php
@@ -5,7 +5,7 @@ include 'class.bag.php';
 header('Content-Type: application/json');
 $bag = new bag();
 $list=$bag->get_bag();
-echo "{\"PRODUCTS\":";
+echo "{\"bag\":";
 echo json_encode($list);
 echo "}";
 ?>
