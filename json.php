a063340c2aec404780558f1d2e1bc91c712273e7
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-11 13:34:57 +0800

    Update json.php

--- json.php
+++ json.php
@@ -3,8 +3,8 @@ include 'config.php';
 include 'class.car.php';
 header('Content-Type: application/json');
 $car = new car();
-$list=$car->get_tbl_car();
-echo "{\"car\":";
+$list=$car->get_ID();
+echo "{\"ID\":";
 echo json_encode($list);
 echo "}";
 ?>
