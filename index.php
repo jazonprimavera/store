8319d3a52f17bc0d9c946278eb35e23659c7240e
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-11 14:38:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+$json = file_get_contents('https://fonzjunsay.herokuapp.com/json.php'); 
+
+$data = json_decode($json,true);
+$list = $data['bag'];
+?>
+<h1> Bags </h1>
+<?php
+foreach($list as $value){
+    ?>
+    <ul>
+        <h2><?php echo $value['brand'];?></h2>
+        <li>description: <?php echo $value['description'];?></li>
+        <li>capacity: <?php echo $value['capacity'];?></li>
+        <li>color: <?php echo $value['color'];?></li>
+    </ul>
+ 
+<?php
+}
+?>
---
+++ json.php
@@ -0,0 +1,11 @@
+
+<?php 
+include 'config.php';
+include 'class.bag.php';
+header('Content-Type: application/json');
+$bag = new bag();
+$list=$bag->get_bag();
+echo "{\"bag\":";
+echo json_encode($list);
+echo "}";
+?>
